1709d582a0444ba92c3a8bd31fdffd332354822f
onikitiuk <oleksandranikitiuk@gmail.com>
2022-07-25 23:29:13 +0200

    implementation SignIn Page

--- src/test/java/pages/SignInPage.java
+++ src/test/java/pages/SignInPage.java
@@ -7,49 +7,39 @@ import org.openqa.selenium.support.PageFactory;
 
 public class SignInPage {
 
-        private final WebDriver driver;
-        private String userEmail;
-        private String userPassword;
+    private final WebDriver driver;
 
-        public SignInPage(WebDriver driver) {
-            this.driver = driver;
-            PageFactory.initElements(driver, this);
-        }
-
-        @FindBy(xpath = "//input[@name='email']")
-        WebElement userEmailField;
-
-        @FindBy(xpath = "//input[@name='password']")
-        WebElement userPasswordField;
-
-        @FindBy(xpath = "//button[@type='submit']")
-        WebElement signInBtn;
-
-        @FindBy(xpath = "//*[@id=\"root\"]/header/div/a")
-        WebElement homeBtn;
-
-
-
-        public WebElement emailId() {
-            return userEmailField;
-        }
-
-        public void setUserEmail(String userEmail) {
-            this.userEmail = userEmail;
-        }
-
-        public WebElement password() {
-            return userPasswordField;
-        }
-        public void setUserPassword(String userPassword) {
-            this.userPassword = userPassword;
-        }
-
-        public WebElement signIn() {
-            return signInBtn;
-        }
-
-        public WebElement home() {
-            return homeBtn;
-        }
+    public SignInPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
+
+    @FindBy(xpath = "//input[@name='email']")
+    private WebElement userEmailInputField;
+
+    @FindBy(xpath = "//input[@name='password']")
+    private WebElement userPasswordInputField;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement signInBtn;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/header/div/a")
+    private WebElement homeBtn;
+
+
+    public void setUserEmailInputField(String email) {
+        userEmailInputField.sendKeys(email);
+    }
+
+    public void setUserPasswordInputField(String password) {
+        userPasswordInputField.sendKeys(password);
+    }
+
+    public void getSignInBtn() {
+        signInBtn.click();
+    }
+
+    public void getHomeBtn() {
+        homeBtn.click();
+    }
+}
