a443fde4d1b3abfc4bc91a2bf6d167a94e791bb0
onikitiuk <oleksandranikitiuk@gmail.com>
2022-08-04 13:13:23 +0200

    Refactored Administrator Page due to project structure changes

--- src/test/java/pages/administrator/AdministratorPage.java
+++ src/test/java/pages/administrator/AdministratorPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import pageComponents.HeaderGeneralPageComponent;
 
 public class AdministratorPage {
 
@@ -15,6 +16,10 @@ public class AdministratorPage {
     public AdministratorPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
+    }
+
+    public HeaderGeneralPageComponent getHeaderGeneralPageComponent() {
+        return new HeaderGeneralPageComponent(driver);
     }
 
     public TabPanelPage getTabPanelPage() {
