910fd616c8276551c48a2b4bc082f0d7ee2c0a50
onikitiuk <oleksandranikitiuk@gmail.com>
2022-08-04 13:13:49 +0200

    Refactored SignIn Page due to project structure changes

--- src/test/java/pages/SignInPage.java
+++ src/test/java/pages/SignInPage.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import pageComponents.HeaderGeneralPageComponent;
 
 public class SignInPage {
-    public SignInPage(WebDriver driver) {
-        PageFactory.initElements(driver, this);
-    }
+
+    private final WebDriver driver;
+
     @FindBy(xpath = "//input[@name='email']")
     private WebElement userEmailInputField;
 
@@ -18,9 +19,14 @@ public class SignInPage {
     @FindBy(xpath = "//button[@type='submit']")
     private WebElement signInBtn;
 
-    @FindBy(xpath = "//*[@id=\"root\"]/header/div/a")
-    private WebElement homeBtn;
+    public SignInPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
 
+    public HeaderGeneralPageComponent getHeaderGeneralPageComponent() {
+        return new HeaderGeneralPageComponent(driver);
+    }
 
     public void setUserEmailInputField(String email) {
         userEmailInputField.sendKeys(email);
@@ -32,9 +38,5 @@ public class SignInPage {
 
     public void clickSignInBtn() {
         signInBtn.click();
-    }
-
-    public void clickHomeBtn() {
-        homeBtn.click();
     }
 }
