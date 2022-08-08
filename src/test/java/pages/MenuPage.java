2e3c71216a391e1482077a2e43ce2fdf648c7797
[Greg] <[kislenko.g@gmail.com]>
2022-08-08 15:21:06 +0300

    Fixed no 'no such element exception', added menuItem page component

--- src/test/java/pages/MenuPage.java
+++ src/test/java/pages/MenuPage.java
@@ -6,23 +6,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import pageComponents.HeaderGeneralPageComponent;
+import pageComponents.menu.MenuItemsPageComponent;
 import pageComponents.menu.OrderConfirmationPageComponent;
 
 import java.util.List;
 
 public class MenuPage {
-    private WebElement neededItemBlock;
-    private List<WebElement> itemAttributeList;
 
     public final HeaderGeneralPageComponent headerGlobal;
-
     public final OrderConfirmationPageComponent orderConfirmation;
-
-    @FindBy(xpath = "//div[contains(@class, 'direction')]/div")
-    private List<WebElement> itemList;
-
-    @FindBy(xpath = "//button[@aria-label='Add to cart']")
-    private List<WebElement> setOfAddToCartBtn;
+    public final MenuItemsPageComponent menuItems;
 
     @FindBy(xpath = "//span[contains(text(), 'Submit order')]")
     private WebElement submitOrder;
@@ -32,49 +25,10 @@ public class MenuPage {
         PageFactory.initElements(driver, this);
         headerGlobal = new HeaderGeneralPageComponent(driver);
         orderConfirmation = new OrderConfirmationPageComponent(driver);
-    }
-
-    private void getBlockByItemName(String itemName)  {
-        for(WebElement item : itemList) {
-            if(item.findElement(By.xpath(
-                    ".//div[contains(@class, 'align-items')]//span"))
-                    .getText()
-                    .equals(itemName))  {
-                neededItemBlock = item;
-                itemAttributeList = item.findElements(By.xpath(".//p"));
-            }
-        }
-    }
-
-    public String getFoodMassByItemName(String itemName) {
-        getBlockByItemName(itemName);
-        return itemAttributeList.get(0).getText();
-    }
-
-    public String getPriceByItemName(String itemName) {
-        getBlockByItemName(itemName);
-        return itemAttributeList.get(1).getText();
-    }
-
-    public String getQuantityByItemName(String itemName) {
-        getBlockByItemName(itemName);
-        return neededItemBlock.findElement(By.xpath(
-                ".//input")).getAttribute("value");
-    }
-
-    public String getFinalPriceByItemName(String itemName) {
-        getBlockByItemName(itemName);
-        return itemAttributeList.get(3).getText();
+        menuItems = new MenuItemsPageComponent(driver);
     }
 
     public void submitOrder() {
         submitOrder.click();
-    }
-
-    public void addToCartByItemName(String itemName) {
-        getBlockByItemName(itemName);
-        neededItemBlock.findElement(By.xpath(
-                ".//button[contains(@class, 'addButton')]"))
-                .click();
     }
 }
