0d84f7e52c9be418d864690b7b11a025d1479706
onikitiuk <oleksandranikitiuk@gmail.com>
2022-09-08 01:04:29 +0200

    fix

--- src/test/java/pages/administrator/order/OrderComponent.java
+++ src/test/java/pages/administrator/order/OrderComponent.java
@@ -9,8 +9,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.List;
 
-import static pages.Main.driver;
-
 public class OrderComponent {
 
     private final WebElement orderDiv;
